787884ec122ee0f8e9040fe47798e91143a4e1a1
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-01 16:36:16 -0500

    products create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,6 @@
 class ProductsController < ApplicationController
-
   def show
-    @product = Product.find_by(id: params["id"])
+    @product = Product.find_by(id: params[:id])
     render template: "products/show"
   end
 
@@ -10,4 +9,21 @@ class ProductsController < ApplicationController
     render template: "products/index"
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render template: "products/show"
+    else
+      render json: {message: "ERROR"}
+    end
+  end
+  
+  def product_params
+    {
+      name: params[:name], 
+      price: params[:price], 
+      image_url: params[:image_url], 
+      description: params[:description]
+    }
+  end
 end
