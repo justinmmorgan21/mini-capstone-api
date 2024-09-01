5c41deea14de378e6181d65a91713c749010ba7f
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-01 16:04:21 -0500

    products index and show actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
-  def get_product
-    @product = Product.first
+  def show
+    @product = Product.find_by(id: params["id"])
     render template: "products/show"
   end
 
-  def all_products
+  def index
     @products = Product.all
     render template: "products/index"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/product" => "products#get_product"
-  get "/all_products" => "products#all_products"
+  get "/products/:id" => "products#show"
+  get "/products" => "products#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
