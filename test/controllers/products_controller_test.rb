f602c591a349940ca96ae72df0b6cb50ac7285f1
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-03 19:52:39 -0500

    add tests for index, create, show, update, and delete

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/products.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Product.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "dummy", price: "200", image_url: "http://dummy.com", description: "really cool thing"}
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/products/#{Product.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["name", "price", "image_url", "description", "id"], data.keys
+  end
+
+  test "update" do
+    product = Product.first
+    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+  end
+
+  test "destroy" do
+    assert_difference "Product.count", -1 do
+      delete "/products/#{Product.first.id}.json"
+      assert_response 200
+    end
+  end
 end
